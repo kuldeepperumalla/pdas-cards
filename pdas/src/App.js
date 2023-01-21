131c4c7325dcab80e3f7eb98d26db30741ce1002
kuldeepperumalla <kuldeepperumalla325@gmail.com>
2023-01-21 12:23:13 -0500

    commit #1

---
+++ pdas/src/App.js
@@ -0,0 +1,14 @@
+import ProfileCard from "./ProfileCard";
+
+function App() {
+    return (
+        <div>
+            <div> personal digital Assistants</div>
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+        </div>
+    )
+}
+
+export default App;
