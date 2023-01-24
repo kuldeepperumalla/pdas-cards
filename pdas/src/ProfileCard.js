43b1b8593f90f9da082c292c04e2de32c83eaad1
kuldeepperumalla <kuldeepperumalla325@gmail.com>
2023-01-24 03:14:53 -0500

    commit #6

--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -1,5 +1,21 @@
-function ProfileCard(){
-    return <h1>Profile Card</h1>
+function ProfileCard({ title, handle, image, description}) {
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="pda logo" />
+        </figure>
+      </div>
+
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">{title}</p>
+          <p className="subtitle is-6">{handle}</p>
+        </div>
+        <div className="content">{description}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProfileCard;
